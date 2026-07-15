93789d3badb116010e27b8652059bb693c5690c0
QOM <management@quitordinarymarketing.ch>
2026-07-15 12:14:18 -0400

    Livegang: Site-URL auf Hauptdomain www.quitordinarymarketing.ch

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from "astro/config";
 // QOM Baukasten – statischer Output (kein Server nötig, läuft auf jedem Hosting)
 export default defineConfig({
   output: "static",
-  site: "https://neu.quitordinarymarketing.ch",
+  site: "https://www.quitordinarymarketing.ch",
   trailingSlash: "ignore",
 });
